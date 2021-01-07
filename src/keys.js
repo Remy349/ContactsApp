9255fe4bdfc9eb8c5989fcc9d7fa307f92bd088c
Santiago Moraga Caldera <59157402+Remy349@users.noreply.github.com>
2021-01-06 20:43:16 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'smoraga',
-        password: 'sMoragaCaldera2001Jesus#',
-        database: 'contactsapp'
+        host: '',
+        user: '',
+        password: '',
+        database: ''
     }
 };
